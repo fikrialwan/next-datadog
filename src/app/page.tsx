3196ea044118d9dcd74e52d19d60758bfda9d9e2
fikrialwan <fikriar93@gmail.com>
2024-08-07 22:35:44 +0700

    feat: pretty format json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <pre>{JSON.stringify(data)}</pre>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </main>
   );
 }
